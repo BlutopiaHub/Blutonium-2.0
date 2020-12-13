090595a0dbffda138e42fca2b68bcc49adfcced0
Israel Aristide <44146685+BlutopiaHub@users.noreply.github.com>
2020-12-13 12:16:54 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-TOKEN ='NjI4NjMwNTc5MTA5Mjk4MjIw.XZN_2Q.fWePZM3KUsV3kIDFT_IaBnkoN1U' ## main client: "NjI3NTIxNDc4NzYxMzgxOTIw.XY926w.mSD4jonhO9AmwsVxTuNCYtWoFeU" ## beta Client: 
-OWNERID = 393165866285662208
+TOKEN ='token' ## main client: "NjI3NTIxNDc4NzYxMzgxOTIw.XY926w.mSD4jonhO9AmwsVxTuNCYtWoFeU" ## beta Client: 
+OWNERID = 00000000000000000000
 
-PgName = 'blutonium'
-PgDb = 'betonium'
-PgPwrd = 'train'
+PgName = 'blu'
+PgDb = 'bet'
+PgPwrd = 'pw'
 PgHost = 'localhost'
 
 clientVer = "2.0-Beta0.27"
