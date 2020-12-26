e40c3a5a81035e031812570e19a056f6213bba73
Israel <israelmaristide@gmail.com>
2020-12-26 05:01:36 -0500

    RIP token

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-TOKEN = 'NjI4NjMwNTc5MTA5Mjk4MjIw.XZN_2Q.C9chj50usQa55icgrf9UL57SIfQ'
-OWNERID = 393165866285662208
+TOKEN = 'token'
+OWNERID = 3000
 
-PgName = 'blutonium'
-PgDb = 'betonium'
-PgPwrd = 'train'
-PgHost = 'localhost'
+PgName = 'bl'
+PgDb = 'be'
+PgPwrd = 't'
+PgHost = 'lt'
 
 clientVer = "2.0-Beta0.29"
 
